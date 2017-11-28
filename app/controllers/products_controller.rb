56db425c1facc050f3708e1d3749013c8429dd01
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-28 21:42:03 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
 
   def add
     raise params.inspect
+    cart << params[:product]
     # @product = Product.find(params[:id])
     # redirect_to @product
   end
